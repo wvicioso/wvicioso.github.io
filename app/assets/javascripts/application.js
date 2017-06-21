233c117ae27e4b19f25ea6ddcfd33bd691be681e
Wellington Vicioso <wellington.vicioso@gmail.com>
2017-06-20 20:08:24 -0400

    remove useless js links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
 
-$(document).ready(function(){
-   $('.parallax').parallax();
-});
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+// });
